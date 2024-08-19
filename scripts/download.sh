bca88b3878d400557bdc5d481b390db82d943252
Kedreamix <1016617094@qq.com>
2024-08-19 01:17:32 +0100

    update download scripts

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,5 +1,5 @@
 # 下载 wav2vec2 模型并保存到指定路径，如果文件已经存在，则跳过下载
-wget -nc https://download.pytorch.org/torchaudio/models/wav2vec2_fairseq_base_ls960_asr_ls960.pth \
+mkdir -p models/ASR/whisper & wget -nc https://download.pytorch.org/torchaudio/models/wav2vec2_fairseq_base_ls960_asr_ls960.pth \
     -O models/ASR/whisper/wav2vec2_fairseq_base_ls960_asr_ls960.pth
 
 # 执行下载脚本
--- scripts/modelscope_download.py
+++ scripts/modelscope_download.py
@@ -8,10 +8,10 @@ snapshot_download('AI-ModelScope/XTTS-v2', local_dir='models/TTS/XTTS-v2')
 # snapshot_download('iic/CosyVoice-300M', local_dir='models/TTS/CosyVoice-300M')
 
 # https://modelscope.cn/models/qwen/qwen1.5-4b-chat
-# snapshot_download('qwen/Qwen1.5-4B-Chat', local_dir='models/LLM/Qwen1.5-4B-Chat')
+snapshot_download('qwen/Qwen1.5-4B-Chat', local_dir='models/LLM/Qwen1.5-4B-Chat')
 
 # https://modelscope.cn/models/qwen/Qwen1.5-1.8B-Chat
-snapshot_download('qwen/Qwen1.5-1.B-Chat', local_dir='models/LLM/Qwen1.5-1.8B-Chat')
+# snapshot_download('qwen/Qwen1.5-1.8B-Chat', local_dir='models/LLM/Qwen1.5-1.8B-Chat')
 
 # https://modelscope.cn/models/keepitsimple/faster-whisper-large-v3
 snapshot_download('keepitsimple/faster-whisper-large-v3', local_dir='models/ASR/whisper/faster-whisper-large-v3')
